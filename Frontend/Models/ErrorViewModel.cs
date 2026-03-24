90af8c343e7c4009373f7dbe051c7cfbf57e43de
Mufliha K P <kpmufliha1@gmail.com>
2026-03-25 00:43:58 +0530

    Delete Frontend directory

--- Frontend/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WatchMyExams.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
